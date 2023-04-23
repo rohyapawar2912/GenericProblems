e4c20bb139556bfc2b3478ca50eeea6fb617cf90
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 08:54:21 +0530

    add :  Refactor2 files

---
+++ Refactor2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practicer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumFinder<int> intMaxFinder = new MaximumFinder<int>(5, 2, 9);
+            Console.WriteLine(intMaxFinder.FindMaximum());
+
+            MaximumFinder<string> stringMaxFinder = new MaximumFinder<string>("apple", "banana", "orange");
+            Console.WriteLine(stringMaxFinder.FindMaximum());
+        }
+    }
+}
