ebd2ab4d2ded220f49616086b3c7f9e549033299
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 09:00:53 +0530

    add : Day 13.5 files

---
+++ Day13.5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace practice5
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 6, 2, 8, 1 };
+            PrintMax(Maximum(numbers));
+
+            string[] words = { "apple", "banana", "orange", "pear" };
+            PrintMax(Maximum(words));
+        }
+
+        static T Maximum<T>(T[] arr) where T : IComparable<T>
+        {
+            if (arr.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty");
+            }
+
+            T max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i].CompareTo(max) > 0)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        static void PrintMax<T>(T max)
+        {
+            Console.WriteLine($"Maximum value: {max}");
+        }
+    }
+
+}
