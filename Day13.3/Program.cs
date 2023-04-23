770bf501aa9d2f155db75e637c9d10aa7a2abc62
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 08:37:47 +0530

    add : Day 13.3 files

---
+++ Day13.3/Program.cs
@@ -0,0 +1,21 @@
+﻿static string FindMax(string a, string b, string c)
+{
+    string max = "";
+    if (a.CompareTo(max) > 0)
+    {
+        max = a;
+    }
+    if (b.CompareTo(max) > 0)
+    {
+        max = b;
+    }
+    if (c.CompareTo(max) > 0)
+    {
+        max = c;
+    }
+    return max;
+}
+// Test case 1: Max string is at first position
+string max1 = FindMax("Apple", "Peach", "Banana");
+Console.WriteLine("Max string is: " + max1); // Output: Max string is: Peach
+
