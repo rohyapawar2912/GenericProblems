127b30c2cd11a9e95cbdab5d8e6879f6026da38d
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 08:47:16 +0530

    add :  Refactor1 files

---
+++ Refactor1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Practicer1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 6, 2, 8, 1 };
+            Console.WriteLine(Maximum(numbers));
+
+            string[] words = { "apple", "banana", "orange", "pear" };
+            Console.WriteLine(Maximum(words));
+        }
+
+        static T Maximum<T>(T[] arr) where T : IComparable<T>
+        {
+            if (arr.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty");
+            }
+
+            T max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i].CompareTo(max) > 0)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
